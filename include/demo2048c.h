4c73f80d38cf764b0d0172f035adb6932c444d75
gitforhzc <clannadhzc@hotmail.com>
2016-07-18 08:14:32 +0800

    add function

--- include/demo2048c.h
+++ include/demo2048c.h
@@ -1,28 +1,33 @@
 /*************************************************************************
-	> File Name: demo2048c.h
-	> Author: 
-	> Mail: 
-	> Created Time: 2016年07月11日 星期一 02时12分45秒
+        > File Name: demo2048c.h
+        > Author:
+        > Mail:
+        > Created Time: 2016年07月11日 星期一 02时12分45秒
  ************************************************************************/
 
 #ifndef _DEMO2048C_H
 
 /*
-	global variable
+        global variable
 */
 // 4*4方格
 int a[4][4];
 // 方格里空格的个数
-int empty;
+int empty, movable;
 int old_y, old_x;
 
 /*
-	global function
+        global function
 */
 void draw();
 void play();
 void init();
+void init_number(int number);
 void draw_one(int y, int x);
+void left();
+void right();
+void up();
+void down();
 void cnt_value(int *new_y, int *new_x);
 int game_over();
 int cnt_one(int y, int x);
--- include/stdafx.h
+++ include/stdafx.h
@@ -1,18 +1,19 @@
 /*************************************************************************
-	> File Name: stdafx.h
-	> Author: 
-	> Mail: 
-	> Created Time: 2016年07月11日 星期一 02时14分05秒
+        > File Name: stdafx.h
+        > Author:
+        > Mail:
+        > Created Time: 2016年07月11日 星期一 02时14分05秒
  ************************************************************************/
 
 #ifndef _STDAFX_H
 
+#include <curses.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <curses.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <signal.h>
 
 #define _STDAFX_H
 #endif
